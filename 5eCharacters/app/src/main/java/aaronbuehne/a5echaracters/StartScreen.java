7913b1c5d1a491c70ba9a09b76012602cc0bba1a
buehneat <buehneat@vcu.edu>
2017-11-11 19:14:44 -0500

    Created Start Screen and New Race Screen

--- 5eCharacters/app/src/main/java/aaronbuehne/a5echaracters/StartScreen.java
+++ 5eCharacters/app/src/main/java/aaronbuehne/a5echaracters/StartScreen.java
@@ -1,7 +1,9 @@
 package aaronbuehne.a5echaracters;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+    }
+
+    public void goNew(View view)
+    {
+        Intent intent = new Intent(this, newRace.class);
+        startActivity(intent);
     }
 }
